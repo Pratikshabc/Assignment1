6ebf52b550aa44c4a8a2685d6ffccb5573478962
Pratikshabc <pratikshachandaragi35@gmail.com>
2023-07-19 17:58:45 +0530

    Added Main class

--- src/pack1/Main.java
+++ src/pack1/Main.java
@@ -21,48 +21,75 @@ class Honda extends Car{
 
     public void color (String n)
     {
-        System.out.println("Honda Color is: "+n);
+        System.out.println("Honda Color: "+n);
     }
     public void wheel (String w)
     {
-        System.out.println("Honda wheel is: "+w);
+        System.out.println("Honda wheel: "+w);
     }
     public void engine (String e)
     {
-        System.out.println("Honda engine is: "+e);
+        System.out.println("Honda engine: "+e);
     }
 }
 class Hyundai extends Car{
     public void color(String n)
     {
-        System.out.println("Hyundai Color is: "+n);
+        System.out.println("Hyundai Color: "+n);
     }
     public void wheel(String w)
     {
-        System.out.println("Hyundai wheel is: "+w);
+        System.out.println("Hyundai wheel: "+w);
     }
     public void engine(String e)
     {
-        System.out.println("Hyundai engine is: "+e);
+        System.out.println("Hyundai engine: "+e);
     }
 
 }
 class Toyota extends Car{
     public void color(String n)
     {
-        System.out.println("Hyundai Color is: "+n);
+        System.out.println("Toyota Color: "+n);
     }
     public void wheel(String w)
     {
-        System.out.println("Hyundai wheel is: "+w);
+        System.out.println("Toyota wheel: "+w);
     }
     public void engine(String e)
     {
-        System.out.println("Hyundai engine is: "+e);
+        System.out.println("Toyota engine: "+e);
     }
 
 }
 
+class Main
+{
+    public static void main(String[] args)
+    {
+        Car h=new Honda();
+        h.accelerator();
+        h.brake();
+        h.clutch();
+        h.color("White");
+        h.wheel("Steel Wheels");
+        h.engine("Hybrid engine");
+        Car hy=new Hyundai();
+        hy.accelerator();
+        hy.brake();
+        hy.clutch();
+        hy.color("Grey");
+        hy.wheel("Alloy Wheels");
+        hy.engine("Electric engine");
+        Car t=new Toyota();
+        t.accelerator();
+        t.brake();
+        t.clutch();
+        t.color("Black");
+        t.wheel("Alloy Wheels");
+        t.engine("Hybrid engine");
+    }
+}
 
 
 
